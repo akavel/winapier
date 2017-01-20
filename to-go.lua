0713f97220759e81f90c77b1992a1fe2759c0a2c
Mateusz Czapliński <czapkofan@gmail.com>
2017-01-20 22:43:48 +0100

    to-lua: add initial types

--- to-go.lua
+++ to-go.lua
@@ -11,6 +11,39 @@ function main()
 		return io.read()
 	end
 	local input = assert(load(read))()
+
+	-- emit entities
+	for name, entity in pairs(input) do
+		if entity.nameKind == 'struct' then
+			-- TODO(akavel): check if using lustache or some other (Go-like?) templating engine simplifies stuff
+			printf("type %s struct {\n", entity.name)
+			for _, m in ipairs(entity.members) do
+				printf("\t%s %s\n", m.name, format_type(m.typ))
+			end
+			printf("}\n")
+		elseif entity.nameKind == 'typedef' then
+			printf("type %s %s\n", entity.name, format_type(entity.typ))
+		else
+			error('unknown entity nameKind: '..entity.nameKind)
+		end
+	end
+end
+
+function format_type(typ)
+	if typ.kind == 'builtin' then
+		local builtins = {int32='int32', int16='int16'}
+		return assert(builtins[typ.builtin], 'unknown builtin: '..typ.builtin)
+	elseif typ.kind == 'pointer' then
+		return '*' .. format_type(typ.to)
+	elseif typ.kind == 'name' then
+		return typ.name
+	else
+		error('unknown type kind: '..typ.kind)
+	end
+end
+
+function printf(fmt, ...)
+	io.write(fmt:format(...))
 end
 
 main()
